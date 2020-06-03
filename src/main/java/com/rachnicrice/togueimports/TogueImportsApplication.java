d8898cf4dd6ac27ba4d96a506618feab95f74ec0
Rachael Rice <Rachnicrice@gmail.com>
2020-06-03 11:27:18 -0400

    first commit

---
+++ src/main/java/com/rachnicrice/togueimports/TogueImportsApplication.java
@@ -0,0 +1,13 @@
+package com.rachnicrice.togueimports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogueImportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TogueImportsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rachnicrice/togueimports/TogueImportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rachnicrice.togueimports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogueImportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
